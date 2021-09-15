c8fd69dff4569cc01f8aa19835f1926dfaf3f04e
Caroline Tan <carolinectan3@gmail.com>
2021-09-14 20:52:42 -0400

    Add uniqueness validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  validates :email, presence: true
-  validates :google_id, presence: true
-  validates :google_token, presence: true
-  validates :google_image_url, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :google_id, presence: true, uniqueness: true
+  validates :google_token, presence: true, uniqueness: true
+  validates :google_image_url, presence: true, uniqueness: true
   validates :zip_code, presence: true
   validates :summary, presence: true
   validates :goal, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,12 +6,16 @@ RSpec.describe User, type: :model do
   #   it { should have_many(:) }
   #   it { should have_many(:).through(:) }
   # end
-  
+
   describe 'validations' do
     it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:google_id) }
+    it { should validate_uniqueness_of(:google_id) }
     it { should validate_presence_of(:google_token) }
+    it { should validate_uniqueness_of(:google_token) }
     it { should validate_presence_of(:google_image_url) }
+    it { should validate_uniqueness_of(:google_image_url) } #uniqueness?
     it { should validate_presence_of(:zip_code) }
     it { should validate_presence_of(:summary) }
     it { should validate_presence_of(:goal) }
