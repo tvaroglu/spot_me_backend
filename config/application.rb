f94a3cc6de1ae9f566a2ffd056059aee2eb63238
Scott Borecki <scottborecki@gmail.com>
2021-09-17 17:48:46 -0600

    Move FactoryBot config to only applicable environments

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,5 @@ module SpotMeBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.factory_bot.definition_file_paths = ['spec/factories']
   end
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,4 +43,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.factory_bot.definition_file_paths = ['spec/factories']
 end
