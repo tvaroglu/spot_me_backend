9b9cb10522c5fd92be1c22d8e0bdb2d6445d902a
Scott Borecki <scottborecki@gmail.com>
2021-09-17 07:04:01 -0600

    Create Users::Gyms index endpoint

---
+++ app/controllers/api/v1/users/gyms_controller.rb
@@ -0,0 +1,9 @@
+# See spec/concerns/response.rb for #json_response and #json_error_response helpers
+class Api::V1::Users::GymsController < ApplicationController
+  def index
+    user = User.find(params[:user_id])
+    gyms = user.gyms
+
+    render json: GymSerializer.new(gyms).serializable_hash, status: :ok
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:show] do
         resources :events, only: [:index], controller: 'users/events'
+        resources :gyms, only: [:index], controller: 'users/gyms'
       end
     end
   end
