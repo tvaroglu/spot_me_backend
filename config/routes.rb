0b2d604d45c6c14e6386af9a49fc2afc7a6990ba
Scott Borecki <scottborecki@gmail.com>
2021-09-17 07:04:26 -0600

    Create Users::Friendships index endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:show] do
         resources :events, only: [:index], controller: 'users/events'
+        resources :friendships, only: [:index], controller: 'users/friendships'
         resources :gyms, only: [:index], controller: 'users/gyms'
       end
     end
