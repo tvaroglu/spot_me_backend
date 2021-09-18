8548cb97cd504e22b2e46d79384bb2d2041d6ce7
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-18 15:09:53 -0400

    Create gym search controller with index action and before action method

---
+++ app/controllers/api/v1/gym_search_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::GymSearchController < ApplicationController
+  before_action :find_gyms, only: [:index]
+
+  def index
+    render json: FoundGymSerializer.new(find_gyms).serializable_hash, status: :ok
+  end
+
+  private
+
+  def find_gyms
+    json_error_response({}, :bad_request) if params[:zip_code].length != 5
+    GymSearchFacade.query_yelp(params[:zip_code]) if params[:zip_code]
+  end
+end
