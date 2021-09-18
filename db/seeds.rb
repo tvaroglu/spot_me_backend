3d4fb45657a2b1d14d316d046509fa2cf8fe55b5
Caroline Tan <carolinectan3@gmail.com>
2021-09-17 22:13:06 -0400

    Add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = create(:user, availability_morning: true)
+user2 = create(:user, availability_morning: true)
+user3 = create(:user, availability_morning: true, availability_afternoon: true)
+user4 = create(:user, availability_morning: true, availability_afternoon: true)
+user5 = create(:user, availability_morning: true, availability_afternoon: true)
+user6 = create(:user, availability_morning: true, availability_evening: true)
+user7 = create(:user, availability_evening: true)
+user8 = create(:user, availability_morning: true, availability_morning: true, availability_evening: true)
+user9 = create(:user, availability_afternoon: true, availability_evening: true)
+user10 = create(:user, availability_evening: true)
