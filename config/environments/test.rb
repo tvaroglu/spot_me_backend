8370a6368bf5e67e97dba39023aae3803d3242a1
Caroline Tan <carolinectan3@gmail.com>
2021-09-17 21:26:12 -0400

    Move factory_bot and faker to all environments

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,6 +43,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  config.factory_bot.definition_file_paths = ['spec/factories']
 end
