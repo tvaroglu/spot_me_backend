aa8f0b563f2904942b6e9fc01e07b5b07187f584
Scott Borecki <scottborecki@gmail.com>
2021-09-19 13:22:57 -0600

    Add Create Friendship endpoint

--- app/controllers/api/v1/users/friendships_controller.rb
+++ app/controllers/api/v1/users/friendships_controller.rb
@@ -8,10 +8,21 @@ class Api::V1::Users::FriendshipsController < ApplicationController
     render json: UserSerializer.new(friends).serializable_hash, status: :ok
   end
 
+  def create
+    follower = User.find(params[:user_id])
+    followee = User.find(params[:followee_id])
+    friendship = Friendship.create!(follower_id: follower.id,
+                                    followee_id: followee.id)
+
+    render json: FriendshipSerializer.new(friendship).serializable_hash,
+           status: :created
+  end
+
   def destroy
     follower = User.find(params[:user_id])
     followee = User.find(params[:id])
-    friendship = Friendship.find_by(follower_id: follower.id, followee_id: followee.id)
+    friendship = Friendship.find_by(follower_id: follower.id,
+                                    followee_id: followee.id)
     friendship.destroy!
   end
 end
--- spec/requests/api/v1/users/friendships/create_request_spec.rb
+++ spec/requests/api/v1/users/friendships/create_request_spec.rb
@@ -1,49 +1,69 @@
 require 'rails_helper'
 
-# See spec/support/requests_shared_examples.rb for shared examples
+# See spec/support/shared_examples/ for shared examples for tests where
+# `include_examples` is used. See Shared Examples in the RSpec docs for more
+# info:
+#   https://relishapp.com/rspec/rspec-core/docs/example-groups/shared-examples
+#
+# See spec/support/request_spec_helper.rb for #json and #json_data helpers.
 describe 'Users::Friendships API', type: :request do
-  # describe 'POST /api/v1/users/:user_id/friendships' do
-  #   let!(:user) { create(:user) }
-  #   let!(:friend) { create(:user) }
-  #   let(:valid_attributes) do
-  #     # TODO: follower_id should come from the path.  The followee_id should be
-  #     #       provided as a query param.
-  #     {
-  #       followee_id: friend.id
-  #     }
-  #   end
-  #
-  #   context 'when the request is valid' do
-  #     before { post "/api/v1/users/#{user.id}/friendships", params: valid_attributes }
-  #
-  #     xit 'creates a gym member', :aggregate_failures do
-  #       expect(json).not_to be_empty
-  #       expect(json_data.size).to eq(3)
-  #       expect(json_data[:attributes][:gym_id]).to eq(gym.id)
-  #       expect(json_data[:attributes][:user_id]).to eq(user.id)
-  #     end
-  #
-  #     include_examples 'status code 201'
-  #   end
-  #
-  #   context 'when the user does not exist' do
-  #     let(:bad_user_id) { User.last.id + 1}
-  #
-  #     before { post "/api/v1/users/#{bad_user_id}/friendships", params: valid_attributes }
-  #
-  #     xit 'returns an error message', :aggregate_failures do
-  #       expect(json).not_to be_empty
-  #       expect(json.size).to eq(2)
-  #
-  #       message = 'your query could not be completed'
-  #       error_message = ["Couldn't find User with 'id'=#{bad_user_id}"]
-  #
-  #       expect(json[:message]).to eq(message)
-  #       expect(json[:errors]).to be_an Array
-  #       expect(json[:errors]).to eq(error_message)
-  #     end
-  #
-  #     include_examples 'status code 404'
-  #   end
-  # end
+  describe 'POST /api/v1/users/:user_id/friendships' do
+    let!(:user) { create(:user) }
+    let!(:friend) { create(:user) }
+    let(:valid_attributes) { { followee_id: friend.id } }
+
+    context 'when the request is valid' do
+      before { post "/api/v1/users/#{user.id}/friendships", params: valid_attributes }
+
+      it 'creates a friendship', :aggregate_failures do
+        expect(json).not_to be_empty
+        expect(json_data.size).to eq(3)
+        expect(json_data[:attributes][:follower_id]).to eq(user.id)
+        expect(json_data[:attributes][:followee_id]).to eq(friend.id)
+      end
+
+      include_examples 'status code 201'
+    end
+
+    context 'when the user does not exist' do
+      let(:bad_user_id) { User.last.id + 1}
+
+      before { post "/api/v1/users/#{bad_user_id}/friendships", params: valid_attributes }
+
+      it 'returns an error message', :aggregate_failures do
+        expect(json).not_to be_empty
+        expect(json.size).to eq(2)
+
+        message = 'your query could not be completed'
+        error_message = ["Couldn't find User with 'id'=#{bad_user_id}"]
+
+        expect(json[:message]).to eq(message)
+        expect(json[:errors]).to be_an Array
+        expect(json[:errors]).to eq(error_message)
+      end
+
+      include_examples 'status code 404'
+    end
+
+    context 'when the friend user does not exist' do
+      let(:bad_user_id) { User.last.id + 1}
+      let(:invalid_attributes) { { followee_id: bad_user_id } }
+
+      before { post "/api/v1/users/#{user.id}/friendships", params: invalid_attributes }
+
+      it 'returns an error message', :aggregate_failures do
+        expect(json).not_to be_empty
+        expect(json.size).to eq(2)
+
+        message = 'your query could not be completed'
+        error_message = ["Couldn't find User with 'id'=#{bad_user_id}"]
+
+        expect(json[:message]).to eq(message)
+        expect(json[:errors]).to be_an Array
+        expect(json[:errors]).to eq(error_message)
+      end
+
+      include_examples 'status code 404'
+    end
+  end
 end
