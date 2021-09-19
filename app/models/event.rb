85ef65c4d14803f9406712bcb697813fd8b8eb5b
Scott Borecki <scottborecki@gmail.com>
2021-09-19 17:56:58 -0600

    Update Event with new schema

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :user
-
-  has_many :invitations, dependent: :destroy
-  has_many :users, through: :invitations
+  belongs_to :gym_membership
 
   validates :user_id, presence: true, numericality: true
-  validates :yelp_gym_id, presence: true
+  validates :gym_membership_id, presence: true, numericality: true
   validates :date_time, presence: true
   validates :activity, presence: true
 end
--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -2,13 +2,7 @@ FactoryBot.define do
   factory :event do
     activity { Faker::Hobby.activity }
     date_time { DateTime.now + rand(1..10).days }
-    yelp_gym_id { Faker::Alphanumeric.alphanumeric(number: 22) }
     user
-  end
-end
-
-def event_with_invitation
-  FactoryBot.create(:event) do |event|
-    FactoryBot.create(:invitation, event: event)
+    gym_membership
   end
 end
