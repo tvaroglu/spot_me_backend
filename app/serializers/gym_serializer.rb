37933aac685d6ff452d8b78871b5ae7f5bc844dc
Taylor Varoglu <tava8398@colorado.edu>
2021-09-30 23:19:09 -0600

    Refactor GymSerializer

--- app/serializers/gym_serializer.rb
+++ app/serializers/gym_serializer.rb
@@ -1,33 +1,22 @@
 class GymSerializer
   class << self
     def format_gyms(gyms)
-      {
-        data: gyms.map do |gym|
-          {
-            id: gym.yelp_gym_id,
-            type: 'gym',
-            attributes: {
-              name: gym.name,
-              address: gym.address,
-              address_details: gym.address_details,
-              phone: gym.phone
-            }
-          }
-        end
-      }
+      { data: gyms.map { |gym| reformat_gym(gym) } }
     end
 
     def format_gym(gym)
+      { data: reformat_gym(gym) }
+    end
+
+    def reformat_gym(gym)
       {
-        data: {
-          id: gym.yelp_gym_id,
-          type: 'gym',
-          attributes: {
-            name: gym.name,
-            address: gym.address,
-            address_details: gym.address_details,
-            phone: gym.phone
-          }
+        id: gym.yelp_gym_id,
+        type: 'gym',
+        attributes: {
+          name: gym.name,
+          address: gym.address,
+          address_details: gym.address_details,
+          phone: gym.phone
         }
       }
     end
