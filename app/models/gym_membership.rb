6b7e5b619a2fc8f6ec193854bafe6fdcc42fda80
Taylor Varoglu <tava8398@colorado.edu>
2021-10-02 21:03:49 -0700

    Add GymMembership.address and GymMembership.address_details to models

--- app/models/gym_membership.rb
+++ app/models/gym_membership.rb
@@ -6,4 +6,16 @@ class GymMembership < ApplicationRecord
   validates :user_id, presence: true, numericality: true
   validates :yelp_gym_id, presence: true
   validates :gym_name, presence: true
+
+  def find_gym
+    GymFacade.find_gym(yelp_gym_id)
+  end
+
+  def address
+    find_gym.address
+  end
+
+  def address_details
+    find_gym.address_details
+  end
 end
--- spec/models/gym_membership_spec.rb
+++ spec/models/gym_membership_spec.rb
@@ -21,4 +21,17 @@ RSpec.describe GymMembership, type: :model do
       end
     end
   end
+
+  describe 'instance methods' do
+    describe '#address and #address_details', :vcr do
+      let!(:yelp_gym) { GymFacade.find_gym('gHmS3WIjRRhSWG4OdCQYLA') }
+      let(:user) { user_with_gym_membership(yelp_gym_id: yelp_gym.yelp_gym_id) }
+      let(:gym_membership) { user.gym_memberships.first }
+
+      it 'can return the address and address details of the gym membership' do
+        expect(gym_membership.address).to eq yelp_gym.address
+        expect(gym_membership.address_details).to eq yelp_gym.address_details
+      end
+    end
+  end
 end
