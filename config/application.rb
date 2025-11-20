cab66cb7c93b9f447ace00077cd580fd785a99d2
Taylor Varoglu <taylor.varoglu@gmail.com>
2025-11-21 07:20:11 +0800

    Update config/application.rb following rails app upgrade

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,12 @@ require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -32,6 +34,6 @@ module SpotMeBackend
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.factory_bot.definition_file_paths = ['spec/factories']
+    # config.factory_bot.definition_file_paths = ['spec/factories']
   end
 end
